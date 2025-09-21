4c659d320b0b5bd7b2901c9b5e3064f9dd3e951c
Sengoku Dev <dev@example.com>
2025-09-21 16:54:45 -0400

    chore(chips): export microchip helpers

--- optipanel/chips/__init__.py
+++ optipanel/chips/__init__.py
@@ -1,11 +1,14 @@
-"""Chip timeframe helpers."""
+"""Timeframe chip helpers."""
 
-from .daily import compute_chips_daily
-from .h60 import compute_chips_h60
-from .m15 import compute_chips_m15
+from .daily import compute_chips_daily, compute_daily_microchips
+from .h60 import compute_chips_h60, compute_h60_microchips
+from .m15 import compute_chips_m15, compute_m15_microchips
 
 __all__ = [
     "compute_chips_daily",
+    "compute_daily_microchips",
     "compute_chips_h60",
+    "compute_h60_microchips",
     "compute_chips_m15",
+    "compute_m15_microchips",
 ]
